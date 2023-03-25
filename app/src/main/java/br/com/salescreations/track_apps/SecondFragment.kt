eec14dab4b6a54358f0cbec9819139e55a74295d
Rafael Sales <rafael.sales93@hotmail.com>
2023-03-25 12:24:12 -0300

    Add first elements material design 3.

--- app/src/main/java/br/com/salescreations/track_apps/SecondFragment.kt
+++ app/src/main/java/br/com/salescreations/track_apps/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import br.com.salescreations.track_apps.databinding.FragmentSecondBinding
 
@@ -13,32 +14,32 @@ import br.com.salescreations.track_apps.databinding.FragmentSecondBinding
  */
 class SecondFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+	private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+	// This property is only valid between onCreateView and
+	// onDestroyView.
+	private val binding get() = _binding!!
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+		_binding = FragmentSecondBinding.inflate(inflater, container, false)
+		return binding.root
 
-    }
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-    }
+		binding.buttonSecond.setOnClickListener {
+			findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+		}
+	}
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
 }
